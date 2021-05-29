1cbb59c1e3f6301ad501b9a45c29af6ad68c21b3
nekyletmbrvll <84419420+nekyletmbrvll@users.noreply.github.com>
2021-05-29 17:26:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,128 @@
+<?php
+// Define recursive function to extract nested values
+function printValues($arr) {
+    global $count;
+    global $values;
+
+    // Check input is an array
+    if(!is_array($arr)){
+        die("ERROR: Input is not an array");
+    }
+
+    /*
+    Loop through array, if value is itself an array recursively call the
+    function else add the value found to the output items array,
+    and increment counter by 1 for each value found
+    */
+    foreach($arr as $key=>$value){
+        if(is_array($value)){
+            printValues($value);
+        } else{
+            $values[] = $value;
+            $count++;
+        }
+    }
+
+    // Return total count and values found in array
+    return array('total' => $count, 'values' => $values);
+}
+
+// Assign JSON encoded string to a PHP variable
+$json = $jsonapi = file_get_contents('http://api.weatherapi.com/v1/current.json?key=a078f06fa64e48b7a4965629212905&q=London&aqi=no');
+// Decode JSON data into PHP associative array format
+$arr = json_decode($json, true);
+
+?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Facebook Login JavaScript Example</title>
-<meta charset="UTF-8">
-</head>
-<body>
-  <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&appId=130525785809675&autoLogAppEvents=1" nonce="oyWXZKal"></script>
-<script>
-
-  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      testAPI();
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this webpage.';
-    }
-  }
-
-
-  function checkLoginState() {               // Called when a person is finished with the Login Button.
-    FB.getLoginStatus(function(response) {   // See the onlogin handler
-      statusChangeCallback(response);
-    });
-  }
-
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '130525785809675', // Your APP ID here
-      cookie     : true,                     // Enable cookies to allow the server to access the session.
-      xfbml      : true,                     // Parse social plugins on this webpage.
-      version    : 'v10.0'           // Use this Graph API version for this call.
-    });
-
-
-    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-      statusChangeCallback(response);        // Returns the login status.
-    });
-  };
-
-  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
-
-</script>
-
-
-<!-- The JS SDK Login Button -->
-<div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="true" data-use-continue-as="false"></div>
-
-
-<div id="status">
-</div>
-</body>
+    <head>
+        <title>Weather API</title>
+        <style>
+        body {
+            font: 600 14px/24px "Open Sans",
+               "HelveticaNeue-Light",
+               "Helvetica Neue Light",
+               "Helvetica Neue",
+               Helvetica, Arial,
+               "Lucida Grande",
+               Sans-Serif;
+         }
+         h1 {
+            color: #312f2f;
+            font-size: 32px;
+            font-weight: bold;
+            margin-bottom: 4px;
+            margin-left: 10px;
+         }
+         h2 {
+            color: #312f2f;
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 4px;
+            margin-left: 10px;
+         }
+         h3 {
+            color: #312f2f;
+            font-size: 14px;
+            font-weight: bold;
+            margin-bottom: 4px;
+            margin-left: 10px;
+         }
+         .container:before, .container:after {
+            content: "";
+            display: table;
+         }
+         .container:after {
+            clear: both;
+         }
+         .container {
+            background: #eaeaed;
+            margin-bottom: 24px;
+            *zoom: 1;
+         }
+         .container-75 {
+            width: 75%;
+         }
+         .container-50 {
+            margin-bottom: 0;
+            width: 50%;
+         }
+         .container, section, aside {
+            border-radius: 6px;
+         }
+         section, aside {
+            background: #b3cde0;
+            color: #000;
+            margin: 1.858736059%;
+            padding: 10px 10px;
+            text-align: left;
+         }
+         section {
+            float: left;
+            width: 30%;
+         }
+         aside {
+            float: right;
+            width: 29.3680297%;
+            border: 1px solid #cad7e3;
+         }
+        </style>
+    </head>
+    <body>
+    <h1>Weather Today</h1>
+        <div class="container">
+           <section>
+           <aside>
+            <img src="<?php echo $arr["current"]["condition"]["icon"];?>"/>
+            <?php echo "<h3>" . $arr["current"]["condition"]["text"] . "</h3>"; ?>
+             </aside>
+            <?php
+            // Print a single value
+            echo "<h1>" . $arr["location"]["name"] . "</h1>";
+            echo "<h2>" . $arr["location"]["country"] . "</h2>";
+            echo "<h3>" . $arr["current"]["temp_c"] . "°C</h3>";
+            ?>
+            </section>
+        </div>
+    </body>
 </html>
